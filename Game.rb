bf112a4dafddde8260beb0a49c152e897e102191
Itokiana <itokianarajoelison@gmail.com>
2018-04-19 23:41:00 +0300

    Modification game 4

--- Game.rb
+++ Game.rb
@@ -43,54 +43,71 @@ class Game
             puts "Veuillez entrer le nom du Joueur O:"
             b = gets.chomp # On demande le nom du second joueur
     
-            @j1.name = a
-            @j2.name = b
+            @j1.name = a # On enregistre le nom du premier joueur
+            @j2.name = b # On enregistre le nom du second joueur
         
             while tour != 10 do
-                @plateau.view_board
+                @plateau.view_board # On affiche le plateau de jeux
                 puts "\n"
-                if tour % 2 == 0
+                if tour % 2 == 0 # Quand c'est le tour du deuxieme joueur
                     puts "Au tour du joueur O (#{@j2.name.capitalize})"
-                    p = gets.chomp
-                    while !@positions[p.upcase.to_sym]  do
+                    p = gets.chomp # On l'invite a saisir une position
+
+                    # Tant que la case n'existe pas, on demande au joueur O de resaisir une position existante
+                    while !@positions[p.upcase.to_sym]  do 
                         puts "Cette position n'existe pas #{@j2.name.capitalize}, veuillez resaisir:" 
                         p = gets.chomp
                     end
+
+                    # Tant que la case n'est pas vide on l'invite a choisir une autre case
                     while @plateau.verify_case(@positions[p.upcase.to_sym]) != " "  do
                         puts "Cette position est deja prise #{@j2.name.capitalize}, veuillez resaisir:" 
                         p = gets.chomp
                     end
+
+                    # On attribue la case au joueur O
                     @plateau.set_position(@positions[p.upcase.to_sym], "O")
-                    if @plateau.game_over("O")
+
+                    # On verifie si le joueur O gagne
+                    if @plateau.game_over("O") # Si oui, on arrete le jeux et le joueur O gagne
                         tour = 10
                         @j2.victory = true
                     else
-                        tour += 1
+                        tour += 1 # Sinon, on passe au tour suivant
                     end
                 else
-                    puts "Au tour du joueur X (#{@j1.name.capitalize})"
-                    p = gets.chomp
+                    puts "Au tour du joueur X (#{@j1.name.capitalize})"# Quand c'est le tour du premier joueur
+                    p = gets.chomp # On l'invite a saisir une position
+
+                    # Tant que la case n'existe pas, on demande au joueur X de resaisir une position existante
                     while !@positions[p.upcase.to_sym] do 
                         puts "Cette position n'existe pas #{@j1.name.capitalize}, veuillez resaisir:" 
                         p = gets.chomp
                     end
+
+                    # Tant que la case n'est pas vide on l'invite a choisir une autre case
                     while @plateau.verify_case(@positions[p.upcase.to_sym]) != " "  do
                         puts "Cette position est deja prise #{@j1.name.capitalize}, veuillez resaisir:" 
                         p = gets.chomp
                     end
+
+                    # On attribue la case au joueur O
                     @plateau.set_position(@positions[p.upcase.to_sym], "X")
-                    if @plateau.game_over("X")
+
+                    # On verifie si le joueur X gagne
+                    if @plateau.game_over("X")# Si oui, on arrete le jeux et le joueur X gagne
                         tour = 10
                         @j1.victory = true
                     else
-                        tour += 1
+                        tour += 1 # Sinon, on passe au tour suivant
                     end
                 end
-                break if @plateau.game_over("X")
             end
         
+            # On affiche le resultat du plateau de jeu final
             @plateau.view_board
         
+            # On check ce qui a gagne ou si c'etait un match nul
             if @j1.victory == true
                 puts "Bravo #{@j1.name.upcase}! T'as gagné !!!!"
             elsif @j2.victory == true
@@ -99,6 +116,7 @@ class Game
                 puts "Match nul!!!!!"
             end
 
+            # On invite les joueurs a faire une autre partie
             puts "\n\nFaire une revenche?\n1- Oui\n2- Non"
             v = gets.chomp
             quit = false
